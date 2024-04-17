22a8d094e73a0342551bc0a9b364b97f16ee522e
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-04-17 00:22:16 -0300

    criando app de perguntas e respostas

--- chatgpt-project/src/App.js
+++ chatgpt-project/src/App.js
@@ -1,9 +1,45 @@
-
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [answer, setAnswer] = useState(" ");
+
+  const client = axios.create({
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_CHATGPT_KEY}`,
+    },
+  });
+
+  const handleSubmit = (e) => {
+    if (e.keyCode === 13 && e.shiftKey === false) {
+      const params = {
+        model: "gpt-3.5-turbo",
+        prompt: e.target.value,
+        max_tokens: 56,
+        temperature: 0.5,
+      };
+
+      client
+        .post("https://api.openai.com/v1/chat/completions", params)
+        .then((result) => setAnswer(result.data.choices[0].text))
+        .catch((err) => console.log(err));
+    }
+  };
+
   return (
     <div className="App">
-     <h1>Projeto</h1>
+      <textarea
+        className="answer-box"
+        id="answer-box"
+        value={answer}
+      ></textarea>
+      <textarea
+        className="text-box"
+        id="text-box"
+        placeholder="pergunte algo..."
+        onKeyDown={(e) => handleSubmit(e)}
+      ></textarea>
     </div>
   );
 }
