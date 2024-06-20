ff5aaa022d91c80a760536efd69ab72d72386f18
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-06-20 20:51:26 -0300

    alteração de layout header

--- chatgpt-project2/src/components/Header.jsx
+++ chatgpt-project2/src/components/Header.jsx
@@ -2,14 +2,14 @@ import { NavLink } from 'react-router-dom';
 
 function Header() {
 	return (
-		<div className=" h-20 flex justify-between items-center bg-blue-800 text-white ">
+		<div className=" h-56 flex justify-between items-center bg-black text-white border-2 border-white p-2 rounded-md">
 			<div className="w-full flex flex-1 gap-2 items-center p-2">
 				<i class="fa-solid fa-robot " ></i>
 				<h2>GERADOR DE PERGUNTAS</h2>
 			</div>
-			<nav className='p-2'>
+			<nav className=' flex justify-between gap-5 p-2'>
 				<ul className="flex justify-between gap-5">
-					<li>
+					<li className={({ isActive }) => isActive ? 'bg-blue-500' : ''}>
 						<NavLink to="/" className={({ isActive }) => isActive ? 'underline' : ''}>
 							<i class="fa-solid fa-house"></i>
 							Home
